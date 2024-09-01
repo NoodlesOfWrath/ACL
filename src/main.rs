b7ec4366c9b2e30de61b2aea900ad2de83fe85fc
Ethan Almloff <76850177+NoodlesOfWrath@users.noreply.github.com>
2024-09-01 13:37:13 -0500

    change name of language
    Also added function support

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ mod ast;
 use ast::*;
 
 #[derive(Parser)]
-#[grammar = "HLHDL.pest"] // Specifies the grammar file
+#[grammar = "ACL.pest"] // Specifies the grammar file
 struct HLHDLParser;
 
 fn main() {
-    let unparsed_file = std::fs::read_to_string("example.hlhdl").expect("cannot read file");
+    let unparsed_file = std::fs::read_to_string("example.acl").expect("cannot read file");
 
     let parse_result = HLHDLParser::parse(Rule::program, &unparsed_file);
 
